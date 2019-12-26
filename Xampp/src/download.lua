ce8efba0fc2e2add0ee882fc7ca27e4c79749b58
Pyutaro <51888687+Pyutaro@users.noreply.github.com>
2019-12-25 18:28:16 -0600

    Update download.lua

--- Xampp/src/download.lua
+++ Xampp/src/download.lua
@@ -4,3 +4,12 @@ if #targs == 0 then
   error("no file specified")
 end
 
+local http_handler = http.get("http://localhost/" .. targs[1] .. ".lua")
+local http_data = http_handler.readAll()
+http_handler.close()
+
+local file_handler = fs.open(targs[1], "wb")
+file_handler.write(http_data)
+file_handler.close()
+
+print("Saved.")
